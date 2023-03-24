870fad1277bc72d59d44e6d27ef6d4c54ed66df1
Aadi P <aadipalnitkar96@gmail.com>
2023-03-24 18:25:12 -0400

    Adding chat script folder to interact with ChatGPT API

---
+++ chat_script/func.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c641854d5f6bcc4b5ec1120420ee2d75c58ed40d12e83f790605d343c811397c
+size 1187
---
+++ chat_script/script.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4f09a50f14f56f5eaf44da27bedce5c44dafe620834e8a78f1999934ee39996c
+size 596
