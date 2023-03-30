6ae973ea7ec8aa5bd534afba5278a2bbdf3e844d
Aadi P <108011872+apalkk@users.noreply.github.com>
2023-03-30 01:09:19 -0400

    Update __init__.py

--- code/range_scanner/__init__.py
+++ code/range_scanner/__init__.py
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:251849038cc2de04356615b193e1430014fae15aeea6258efa51e9510789c535
-size 696
+# context.area: VIEW_3D
+
+# add-on skeleton taken from: https://blender.stackexchange.com/a/57332
+
+bl_info = {
+    "name" : "range_scanner",
+    "author" : "Lorenzo Neumann",
+    "description" : "Range scanner simulation for Blender",
+    "blender" : (2, 81, 0),
+    "version" : (0, 0, 1),
+    "location" : "3D View > Scanner",
+    "warning" : "",
+    "category" : "3D View",
+    "wiki_url": "https://git.informatik.tu-freiberg.de/masterarbeit/blender-range-scanner",
+}
+
+from .ui import user_interface
+
+# register all needed classes on startup
+def register():
+    user_interface.register()
+
+# delete all classes on shutdown
+def unregister():
+    user_interface.unregister()
