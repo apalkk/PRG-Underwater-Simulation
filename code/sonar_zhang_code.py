08e9d22ec188471a69a4516799a48fcaa25b32fe
AP <108011872+apalkk@users.noreply.github.com>
2024-07-02 02:28:12 -0400

    Update sonar_zhang_code.py

--- code/sonar_zhang_code.py
+++ code/sonar_zhang_code.py
@@ -15,6 +15,7 @@ import pyhocon.converter as cvt
 import argparse
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
+
 def load_neus_cams(cam_path):
     """
     Args:
@@ -25,8 +26,8 @@ def load_neus_cams(cam_path):
     poses = []
     processed_poses = np.load(cam_path)
     for k in processed_poses.keys():
-        if "camera_mat_inv" in k: 
-            camera_mat_inv = processed_poses[k] 
+        if "camera_mat_inv" in k:
+            camera_mat_inv = processed_poses[k]
         if "world_mat" in k and "world_mat_inv" not in k:
             world_mat = processed_poses[k]
             poses.append(np.linalg.inv(camera_mat_inv @ world_mat))
@@ -34,11 +35,12 @@ def load_neus_cams(cam_path):
 
     scale_mat = processed_poses["scale_mat_0"]
     scale = scale_mat[0, 0]
-    offset = scale_mat[:3, 3]   
-    poses[:, :3, -1] = (poses[:, :3, -1] - offset) / scale 
-    poses = torch.from_numpy(poses).cuda() 
+    offset = scale_mat[:3, 3]
+    poses[:, :3, -1] = (poses[:, :3, -1] - offset) / scale
+    poses = torch.from_numpy(poses).cuda()
     print(scale, offset)
     return poses, scale, offset
+
 
 def gen_rays_at_sonar_for_proj(
     pose, azi_range, azi_bins, ele_range, pp_arc, **kwargs
@@ -49,19 +51,20 @@ def gen_rays_at_sonar_for_proj(
         rays_d: (azi_bins*pp_arc, 3), numpy array
     """
     azis = torch.linspace(azi_range[0], azi_range[1], azi_bins)
-    eles = torch.linspace(ele_range[0], ele_range[1], pp_arc)  
+    eles = torch.linspace(ele_range[0], ele_range[1], pp_arc)
     pixels_theta, pixels_phi = torch.meshgrid(
         azis, eles, indexing="ij"  # careful with indexing here
     )  # azi_bins, pp_arc
-    xs = -torch.cos(pixels_phi) * torch.sin(pixels_theta) 
+    xs = -torch.cos(pixels_phi) * torch.sin(pixels_theta)
     ys = -torch.sin(pixels_phi)  # sign?
-    zs = torch.cos(pixels_phi) * torch.cos(pixels_theta) 
+    zs = torch.cos(pixels_phi) * torch.cos(pixels_theta)
     p = torch.stack([xs, ys, zs], axis=-1)
 
     rays_v = p / torch.linalg.norm(
         p, ord=2, dim=-1, keepdims=True
     )  # azi_bins, pp_arc, 3
-    rays_v = torch.matmul(pose[None, None, :3, :3], rays_v[..., None]).squeeze()
+    rays_v = torch.matmul(pose[None, None, :3, :3],
+                          rays_v[..., None]).squeeze()
     rays_o = pose[None, :3, 3].expand(
         rays_v.shape
     )  # azi_bins, pp_arc, 3
@@ -73,7 +76,7 @@ def gen_rays_at_sonar(
 ):
     azis = torch.linspace(azi_range[0], azi_range[1], azi_bins)
     rads = torch.linspace(rad_range[0], rad_range[1], rad_bins)
-    eles = torch.linspace(ele_range[0], ele_range[1], pp_arc)  
+    eles = torch.linspace(ele_range[0], ele_range[1], pp_arc)
     pixels_r, pixels_theta, pixels_phi = torch.meshgrid(
         rads, azis, eles, indexing="ij"  # careful with indexing here
     )  # rad_bins, azi_bins, pp_arc
@@ -82,16 +85,17 @@ def gen_rays_at_sonar(
     # how to visualize: align angles in spherical coordinates diagram with
     # sonar expectation for how angles are defined
     # below shares code with gen_random_rays_at_sonar?
-    xs = -pixels_r * torch.cos(pixels_phi) * torch.sin(pixels_theta) 
+    xs = -pixels_r * torch.cos(pixels_phi) * torch.sin(pixels_theta)
     ys = -pixels_r * torch.sin(pixels_phi)  # sign?
-    zs = pixels_r * torch.cos(pixels_phi) * torch.cos(pixels_theta) 
+    zs = pixels_r * torch.cos(pixels_phi) * torch.cos(pixels_theta)
 
     p = torch.stack([xs, ys, zs], axis=-1)
 
     rays_v = p / torch.linalg.norm(
         p, ord=2, dim=-1, keepdims=True
     )  # rad_bins, azi_bins, pp_arc, 3
-    rays_v = torch.matmul(pose[None, None, None, :3, :3], rays_v[..., None]).squeeze()
+    rays_v = torch.matmul(
+        pose[None, None, None, :3, :3], rays_v[..., None]).squeeze()
     rays_o = pose[None, None, :3, 3].expand(
         rays_v.shape
     )  # rad_bins, azi_bins, pp_arc, 3
@@ -106,19 +110,15 @@ def gen_rays_at_sonar(
     return ray_info
 
 
-
 def cvt_blender_to_neus_coords(pts):
-    original_shape = pts.shape 
-    pts = pts.reshape(-1, 3) 
+    original_shape = pts.shape
+    pts = pts.reshape(-1, 3)
     cmat = np.array(
-         [[1.0, 0.0, 0.0], 
-         [0.0, 0.0, -1.0], 
+        [[1.0, 0.0, 0.0],
+         [0.0, 0.0, -1.0],
          [0.0, 1.0, 0.0]])
     out = pts @ cmat.T
     return out.reshape(original_shape)
-
-
-
 
 
 def make_argument_parser():
@@ -132,14 +132,15 @@ def make_argument_parser():
     parser.add_argument("--out_dir", type=str)
     # parser.add_argument("--name", type=str)
     args = parser.parse_args()
-    return args    
+    return args
 
 # TODO: should have made this a function instead of have this weird command line parsing stuff
-#       (cruft leftover from multiprocessing speedup experiments) 
+#       (cruft leftover from multiprocessing speedup experiments)
+
 
 def generate_sonar_image_data_from_args(args):
     gt_mesh_path = args.gt_mesh_path
-    train_data_path = args.train_data_path    
+    train_data_path = args.train_data_path
     out_dir = args.out_dir
     # mesh_name = args.name
     H, W = args.H, args.W
@@ -173,19 +174,20 @@ def as_mesh(scene_or_mesh):
             # we lose texture information here
             mesh = trimesh.util.concatenate(
                 tuple(trimesh.Trimesh(vertices=g.vertices, faces=g.faces)
-                    for g in scene_or_mesh.geometry.values()))
+                      for g in scene_or_mesh.geometry.values()))
     else:
-        assert(isinstance(scene_or_mesh, trimesh.Trimesh))
+        assert (isinstance(scene_or_mesh, trimesh.Trimesh))
         mesh = scene_or_mesh
     return mesh
 
-def generate_sonar_image_data(gt_mesh_path, 
-                              train_data_path, 
-                              out_dir, 
-                              mesh_name, 
+
+def generate_sonar_image_data(gt_mesh_path,
+                              train_data_path,
+                              out_dir,
+                              mesh_name,
                               H, W, vfov, ckpt_path, fix_configs_only=False):
     dataset_name = train_data_path.split("/")[-1]
-    
+
     os.makedirs(out_dir, exist_ok=True)
     os.makedirs(f"{out_dir}/Data", exist_ok=True)
     os.makedirs(f"{out_dir}/imgs", exist_ok=True)
@@ -200,7 +202,7 @@ def generate_sonar_image_data(gt_mesh_path,
         ele_range=[ele_min, ele_max],
         pp_arc=1024,
     )
-    
+
     render_sonar_images(train_data_path, cam_info, out_dir, gt_mesh_path)
 
 
@@ -209,13 +211,15 @@ def render_sonar_images(train_data_path, cam_info, out_dir, gt_mesh_path):
     mesh = as_mesh(mesh)
     mesh.vertices = cvt_blender_to_neus_coords(mesh.vertices)
 
-    poses, scale, offset = load_neus_cams(f"{train_data_path}/cameras_sphere.npz")
-    mesh.vertices = (mesh.vertices - offset) / scale # didn't matter before when these were just dummy values
-                                                     # but now it does with most recent data
+    poses, scale, offset = load_neus_cams(
+        f"{train_data_path}/cameras_sphere.npz")
+    # didn't matter before when these were just dummy values
+    mesh.vertices = (mesh.vertices - offset) / scale
+    # but now it does with most recent data
 
     for i in range(len(poses)):
-        pose = poses[i] 
-        # rotate sonar by 90 degrees, about z (optical) axis 
+        pose = poses[i]
+        # rotate sonar by 90 degrees, about z (optical) axis
         rot = torch.tensor([[0.0, -1.0, 0.0],
                             [1.0, 0.0, 0.0],
                             [0.0, 0.0, 1.0]])
@@ -231,13 +235,13 @@ def render_sonar_images(train_data_path, cam_info, out_dir, gt_mesh_path):
             curr_data["ImagingSonar"] = render_rt
             with open(f"{out_dir}/Data/{i:03d}.pkl", "wb") as f:
                 pickle.dump(curr_data, f)
-    
 
-def sonar_project_hitpoints(locations, 
-                            index_tri, 
-                            index_ray, 
-                            mesh, 
-                            origin, 
+
+def sonar_project_hitpoints(locations,
+                            index_tri,
+                            index_ray,
+                            mesh,
+                            origin,
                             rays_d,
                             cam_info,):
     """
@@ -264,26 +268,28 @@ def sonar_project_hitpoints(locations,
     # mask[r_binned, a_binned] = 1
     np.add.at(total, (r_binned, a_binned), shading)
     np.add.at(counts, (r_binned, a_binned), 1)
-    # remember to average over the number of rays per bin? 
+    # remember to average over the number of rays per bin?
     render_rt[counts > 0] = total[counts > 0] / counts[counts > 0]
     render_rt[counts > 0] = render_rt[counts > 0] / rad_img[counts > 0]
     render_rt_out = (render_rt*255).astype(np.uint8)
 
     return render_rt_out
 
+
 def render_sonar_image(pose, cam_info, mesh):
 
     # below has issues with multiprocessing
-    ray_origins, ray_directions = gen_rays_at_sonar_for_proj(pose, **cam_info)  # rad_bins, azi_bins, pp_arc, 3
-    
+    ray_origins, ray_directions = gen_rays_at_sonar_for_proj(
+        pose, **cam_info)  # rad_bins, azi_bins, pp_arc, 3
+
     locations, index_ray, index_tri = mesh.ray.intersects_location(
         ray_origins=ray_origins,
         ray_directions=ray_directions,
-        multiple_hits=False) # (n, 3), (n,), (n,)
-    
+        multiple_hits=False)  # (n, 3), (n,), (n,)
+
     origin = pose[:3, -1].cpu().numpy()
-    render_rt = sonar_project_hitpoints(locations, 
-                                        index_tri, 
+    render_rt = sonar_project_hitpoints(locations,
+                                        index_tri,
                                         index_ray, mesh, origin, ray_directions, cam_info)
     # print(render_rt.sum())
     return render_rt
@@ -300,16 +306,18 @@ def get_visible_points(mesh, pose):
     t = pose[:3, 3:4].T
     ray_origins = np.tile(t, (len(mesh.vertices), 1))
     ray_directions = mesh.vertices - ray_origins
-    ray_directions = ray_directions / np.linalg.norm(ray_directions, axis=1, keepdims=True) 
+    ray_directions = ray_directions / \
+        np.linalg.norm(ray_directions, axis=1, keepdims=True)
     locations, index_ray, index_tri = mesh.ray.intersects_location(
-            ray_origins=ray_origins,
-            ray_directions=ray_directions,
-            multiple_hits=False) # (n, 3), (n,), (n,)          
+        ray_origins=ray_origins,
+        ray_directions=ray_directions,
+        multiple_hits=False)  # (n, 3), (n,), (n,)
     locations_matched = np.zeros_like(mesh.vertices)
     locations_matched[index_ray] = locations
     distances = np.linalg.norm(locations_matched - mesh.vertices, axis=1)
     visible_points = locations_matched[distances < 1e-6]
-    return visible_points 
+    return visible_points
+
 
 if __name__ == "__main__":
     args = make_argument_parser()
